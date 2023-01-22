19b21bb3c58ec84cc4c65a9e73f7e3554376942a
Nikita <OvchinnikovNE1995@yandex.ru>
2023-01-22 17:27:56 +0300

    1st iteration fixes

--- scripts/index.js
+++ scripts/index.js
@@ -4,23 +4,23 @@ const buttonClose = document.querySelector(".popup__button_act_exit");
 const getName = document.querySelector(".profile__name");
 const getDescription = document.querySelector(".profile__description");
 const editForm = document.querySelector(".popup__container");
-let nameInput = editForm.querySelector(".popup__input_name");
-let descriptionInput = editForm.querySelector(".popup__input_description");
+let nameInput = editForm.querySelector(".popup__input_type_name");
+let descriptionInput = editForm.querySelector(".popup__input_type_description");
 
 function openPopup() {
-  popup.classList.add("popup__opened");
+  popup.classList.add("popup_opened");
   nameInput.value = getName.textContent;
   descriptionInput.value = getDescription.textContent;
 }
 
 function closePopup() {
-  popup.classList.remove("popup__opened");
+  popup.classList.remove("popup_opened");
 }
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  getName.textContent = `${nameInput.value}`;
-  getDescription.textContent = `${descriptionInput.value}`;
+  getName.textContent = nameInput.value;
+  getDescription.textContent = descriptionInput.value;
   closePopup();
 }
 
