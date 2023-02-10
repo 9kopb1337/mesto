6d7d5a0aca21c3cc518f0c243b3a1778bb71c75b
Nikita <OvchinnikovNE1995@yandex.ru>
2023-02-10 15:34:30 +0300

    before popup window for picture

--- scripts/index.js
+++ scripts/index.js
@@ -9,49 +9,49 @@ let descriptionInput = editForm.querySelector(".popup__input_type_description");
 
 const initialCards = [
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
   },
   {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
   },
   {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
   },
   {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
   },
   {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
   },
   {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
 ];
 
-const elementsList = document.querySelector('.elements');
-const elementTemplate = document.querySelector('.elements-template').content;
+const elementsList = document.querySelector(".elements");
+const elementTemplate = document.querySelector(".elements-template").content;
 
 initialCards.forEach((element) => {
   const cardElement = elementTemplate.cloneNode(true);
 
-  cardElement.querySelector('.element__title').textContent = element.name;
-  cardElement.querySelector('.element__picture').src = element.link;
+  cardElement.querySelector(".element__title").textContent = element.name;
+  cardElement.querySelector(".element__picture").src = element.link;
 
   const buttonLike = cardElement.querySelector(".element__like");
-buttonLike.addEventListener("click", function (evt) {
-  evt.target.classList.toggle('element__like_active');
-})
-
-
+  buttonLike.addEventListener("click", function (evt) {
+    evt.target.classList.toggle("element__like_active");
+  });
 
   elementsList.append(cardElement);
-})
+
+
+});
 
 function openEditPopup() {
   popupEditProfile.classList.add("popup_opened");
@@ -74,15 +74,20 @@ buttonEditOpen.addEventListener("click", openEditPopup);
 buttonClose.addEventListener("click", closeEditPopup);
 editForm.addEventListener("submit", editFormSubmitHandler);
 
-
 const popupAddPhoto = document.querySelector(".popup_add_photo");
 const buttonAddOpen = document.querySelector(".profile__button_act_add");
-const buttonClosePhoto = document.querySelector(".popup__button_act_exit_photo");
+const buttonClosePhoto = document.querySelector(
+  ".popup__button_act_exit_photo"
+);
 const getPhotoName = document.querySelector(".element__title");
 const getPhotoLink = document.querySelector(".element__picture");
 const addPhotoForm = document.querySelector(".popup__form_photo");
-let photoNameInput = addPhotoForm.querySelector(".popup__input_type_name_photo");
-let photoLinkInput = addPhotoForm.querySelector(".popup__input_type_photo_link");
+let photoNameInput = addPhotoForm.querySelector(
+  ".popup__input_type_name_photo"
+);
+let photoLinkInput = addPhotoForm.querySelector(
+  ".popup__input_type_photo_link"
+);
 
 function openAddPopup() {
   popupAddPhoto.classList.add("popup_opened");
@@ -95,12 +100,41 @@ function closeAddPopup() {
 function addPhotoSubmitHandler(evt) {
   evt.preventDefault();
   const cardElement = elementTemplate.cloneNode(true);
-  cardElement.querySelector('.element__title').textContent = photoNameInput.value;
-  cardElement.querySelector('.element__picture').src = photoLinkInput.value;
+  cardElement.querySelector(".element__title").textContent =
+    photoNameInput.value;
+  cardElement.querySelector(".element__picture").src = photoLinkInput.value;
+
+  const buttonLike = cardElement.querySelector(".element__like");
+  buttonLike.addEventListener("click", function (evt) {
+    evt.target.classList.toggle("element__like_active");
+  });
+
   elementsList.prepend(cardElement);
+
   closeAddPopup();
+  photoNameInput.value = "";
+  photoLinkInput.value = "";
+
+  const deleteButton = document.querySelector(".element__delete");
+  deleteButton.addEventListener("click", function () {
+    const elementsPhoto = deleteButton.closest(".element");
+    elementsPhoto.remove();
+  });
 }
 
 buttonAddOpen.addEventListener("click", openAddPopup);
 buttonClosePhoto.addEventListener("click", closeAddPopup);
 addPhotoForm.addEventListener("submit", addPhotoSubmitHandler);
+
+
+function removeElementHandler(evt) {
+  evt.preventDefault();
+  const elementsPhoto = deleteButton.closest(".element");
+  elementsPhoto.remove();
+}
+
+
+const deleteButton = document.querySelector(".element__delete");
+deleteButton.addEventListener("click", removeElementHandler);
+
+
