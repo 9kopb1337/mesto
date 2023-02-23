90ccd3f456aa027e85e98b2ca059e50d77782d93
Nikita <OvchinnikovNE1995@yandex.ru>
2023-02-23 14:16:41 +0300

    some

--- scripts/validate.js
+++ scripts/validate.js
@@ -39,9 +39,4 @@ const enableValidation = () => {
  })
 }
 
-const formList = Array.from(document.querySelectorAll(".form"));
-formList.forEach((formElement) => {
-  formElement.addEventListener("submit", enableValidation)
-})
-
 enableValidation();
