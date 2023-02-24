fead28ee015b882a30f1f7c6fcfde6e9874d73a0
Nikita <OvchinnikovNE1995@yandex.ru>
2023-02-24 13:25:01 +0300

    going for 2nd iteration

--- scripts/validate.js
+++ scripts/validate.js
@@ -28,7 +28,7 @@ const checkInputValidity = (formElement, inputElement) => {
   }
 };
 
-const toggleButtonState = (inputList, submitButtonSelector) => {
+const toggleButtonState = (inputList, submitButtonSelector, inactiveButtonClass) => {
   if (hasInvalidInput(inputList)) {
     submitButtonSelector.classList.add("popup__button_inactive");
   } else {
@@ -45,7 +45,12 @@ const hasInvalidInput = (inputList) => {
 const setEventListeners = (formElement) => {
   const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
   const buttonElement = formElement.querySelector('.popup__button_act_submit');
-  toggleButtonState(inputList, buttonElement);
+  toggleButtonState(inputList, buttonElement, config);
+  formElement.addEventListener('reset', () => {
+    setTimeout(() => {
+      toggleButtonState(inputList, buttonElement, config)
+    }, 0);
+  })
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', function () {
       checkInputValidity(formElement, inputElement);
@@ -54,18 +59,11 @@ const setEventListeners = (formElement) => {
   });
 };
 
-const enableValidation = () => {
+const enableValidation = (config) => {
   const formList = Array.from(document.querySelectorAll(".form"));
   formList.forEach((formElement) => {
-    setEventListeners(formElement);
+    setEventListeners(formElement, config);
   });
 };
 
-enableValidation({
-  formSelector: ".form",
-  inputSelector: ".popup__input",
-  submitButtonSelector: ".popup__button_act_submit",
-  inactiveButtonClass: "popup__button_inactive",
-  inputErrorClass: "popup__input_type_error",
-  errorClass: "popup__input-error_active"
-});
+enableValidation(config);
